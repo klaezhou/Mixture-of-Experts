4410a376dd6a6f41c222d287c8319ad661c30c8a
klae <12112228@sustech.edu.cn>
2025-10-22 08:14:33 +0000

    2-D 1 change

--- moe_module/epi_rank.py
+++ moe_module/epi_rank.py
@@ -2,269 +2,215 @@ import torch
 import torch.nn as nn
 import numpy as np
 #numerical integral to compute epi_rank
+# class epi_rank_moe():
+#     def __init__(self, model,interval,num_samples):
+#         """
+#         model: class MOE_Model in moe.py
+#         """
+#         self.model = model
+        
+#         #find the MOE in MOE_Model
+#         # moe example: y,loss=self.moe(x,train)
+#         self.moe = model.moe
+#         self.device=next(model.parameters()).device
+#         self.epsilon =1e-6
+#         interval = eval(interval)  # 例如 "[-1,1]" -> [-1, 1]
+#         self.x = torch.linspace(interval[0], interval[1], num_samples).view(-1, 1).to(self.device)
+#         self.num_samples = num_samples
+#         self.training=False
+#         self.M_weight=self.compute_matrix()
+#         self.rank=0
+        
+        
+#     def compute_matrix(self):
+#         """moe 
+#         input [Batch,1,] -> Gating [Batch,num_experts] * 
+#         Experts [Batch,1,hidden_size] ->
+#         """
+#         d_matrix,loss=self.moe(self.x,self.training)
+#         d_matrix=d_matrix.detach() #[Batch,hidden_size]
+#         #init weight matrix, numerical
+#         def diag_weight(size):
+#             #梯形公式
+#             weights = torch.ones(size)
+#             weights[0] = weights[-1] = 0.5
+#             diag_matrix = torch.diag(weights).to(self.device)
+#             return diag_matrix
+#         weight_matrix=diag_weight(self.num_samples)
+#         #D.T W D
+#         M_weight = torch.matmul(d_matrix.t(), weight_matrix)
+#         M_weight = torch.matmul(M_weight, d_matrix)
+#         # y_np = d_matrix.cpu().numpy()
+#         # print("moe ouput:", y_np.shape)
+#         self.M_weight = M_weight
+#         return  M_weight
+        
+#     def rank_moe(self):
+#         eigvals = torch.linalg.eigvalsh(self.M_weight)
 
+#         # 设定阈值 epsilon
+#         epsilon = 1e-6
+
+#         # 统计大于 epsilon 的特征值数量
+#         count = (eigvals > epsilon).sum().item()
+#         self.rank = count
+#         return count
+        
+        
 class epi_rank_mlp():
-    def __init__(self, model,interval,num_samples,moe_training=True,index=1):
-        """index: defalut 2 ; if no moe, then 1"""
+    def __init__(self, model,x_interval,t_interval,x_num_samples, t_num_samples,epsilon, activation,moe_training=True,index=0):
+        """index: defalut 0 ; if no moe, then 1"""
         self.moe_training=moe_training
         self.model = model
         self.index=index
+        self.activation=activation
         #find the MLP in MOE_Model moe_model => moe-*fcnn-ouputlayer, index=2 表示从第一个fcnn开始计算 
         #self.model.model[:self.n+self.index] 表示到前n+index个fcnn
         
-        self.mlp = [ PartialMOE(model,n,self.index) for n in range(len(model.model)-1)]
+        self.mlp = [ PartialMOE(model,n,self.index) for n in range(len(model.model)-index)]
         self.device=next(model.parameters()).device
-        self.epsilon =1e-3
-        interval = eval(interval)  # 例如 "[-1,1]" -> [-1, 1]
-        self.x = torch.linspace(interval[0], interval[1], num_samples).view(-1, 1).to(self.device)
-        self.num_samples = num_samples
-        
+        self.epsilon = epsilon
+
+        x_interval = eval(x_interval)  # 例如 "[-1,1]" -> [-1, 1]
+        self.x = torch.linspace(x_interval[0], x_interval[1], x_num_samples).view(-1, 1).to(self.device)
+        self.x_num_samples = x_num_samples
+
+        t_interval = eval(t_interval)
+        self.t = torch.linspace(t_interval[0], t_interval[1], t_num_samples).view(-1, 1).to(self.device)
+        self.t_num_samples = t_num_samples
+
+        X, T = torch.meshgrid(self.x.squeeze(), self.t.squeeze(), indexing='ij')
+        X_total = torch.stack([X.flatten(), T.flatten()], dim=1)
+        self.X_total = X_total
+
         self.training=False
         self.M_weight_list=self.compute_matrix_list()
+        if moe_training:
+            self.M_weight_list_experts=self.compute_experts_matrix_list()
+        self.rank_list_experts=0
         self.rank_list=0
-    def compute_matrix_list(self):
-                d_matrix_list=[]
-                for i in range(len(self.mlp)):
-                    d_matrix=self.mlp[i](self.x,self.training,self.moe_training)
-                    # print("length",len(self.model.model))
-                    # print(f"mlp {i}ouput:", d_matrix.shape)
-                    d_matrix=d_matrix.detach()
-                    d_matrix_list.append(d_matrix)
 
-                #init weight matrix, numerical
-                def diag_weight(size):
-                    #梯形公式
-                    weights = torch.ones(size)
-                    weights[0] = weights[-1] = 0.5
-                    diag_matrix = torch.diag(weights).to(self.device)
-                    return diag_matrix
-                weight_matrix=diag_weight(self.num_samples)
-                #D.T W D
-                M_weight_list=[]
-                for i in range(len(d_matrix_list)):
-                    M_weight = torch.matmul(d_matrix_list[i].t(), weight_matrix)
-                    M_weight = torch.matmul(M_weight, d_matrix_list[i])
-                    M_weight_list.append(M_weight)
-                self.M_weight_list = M_weight_list
-                # y_np = d_matrix.cpu().numpy()
-                # print("moe ouput:", y_np.shape)
-                return  M_weight_list
-            
+    def compute_matrix_list(self):
+        d_matrix_list=[]
+        for i in range(len(self.mlp)):
+            d_matrix=self.mlp[i](self.X_total,self.training,self.moe_training)
+            d_matrix=d_matrix.detach()
+            d_matrix_list.append(d_matrix)
+
+        #init weight matrix, numerical
+        def diag_weight(x_size, t_size):
+            #梯形公式
+            weights = torch.ones((x_size, t_size))
+            weights[0, :] *= 0.5
+            weights[-1, :] *= 0.5
+            weights[:, 0] *= 0.5
+            weights[:, -1] *= 0.5
+            weights = weights.flatten()
+            diag_matrix = (self.x[-1]-self.x[0])/(x_size-1) * (self.t[-1]-self.t[0])/(t_size-1) * torch.diag(weights).to(self.device)
+            return diag_matrix
+        
+        weight_matrix=diag_weight(self.x_num_samples, self.t_num_samples)
+        #D.T W D
+        M_weight_list=[]
+        for i in range(len(d_matrix_list)):
+            M_weight = torch.matmul(d_matrix_list[i].t(), weight_matrix)
+            M_weight = torch.matmul(M_weight, d_matrix_list[i])
+            M_weight_list.append(M_weight)
+        self.M_weight_list = M_weight_list
+        # y_np = d_matrix.cpu().numpy()
+        # print("moe ouput:", y_np.shape)
+        return  M_weight_list
+        
     def rank_mlp(self):
         rank_list=[]
         for i in range(len(self.M_weight_list)):
             eigvals = torch.linalg.eigvalsh(self.M_weight_list[i])
 
-            # 设定阈值 epsilon
-            epsilon = self.epsilon
-
             # 统计大于 epsilon 的特征值数量
-            count = (eigvals > epsilon).sum().item()
+            count = (eigvals > self.epsilon).sum().item()
             rank_list.append(count)
         self.rank_list = rank_list
         return rank_list
-            
-class epi_rank_expert():
-    def __init__(self, model,interval,num_samples,index=0):
-        """
-        This is a class for computing the rank of an expert in a mixture of experts model. --1D
-        Parameters:
-        model: class MOE_Model in moe.py
-        interval: the interval of the input"
-        num_samples: the number of samples
-        index: the index of the moe layer, defalut 0
-        
-        """
-        self.model=model
-        self.index=index
-        interval = eval(interval)  # 例如 "[-1,1]" -> [-1, 1]
-        self.device=next(model.parameters()).device
-        self.x = torch.linspace(interval[0], interval[1], num_samples).view(-1, 1).to(self.device)
-        self.num_experts=self.model.num_experts
-        self.num_samples = num_samples
-        self.experts=[Expert_output(model,i,self.index) for i in range(self.num_experts)]
-        self.M_weight_list=self.compute_matrix_list()
-        self.M_weight_total=self.compute_total_matrix()
-        self.rank_list=0
-        self.total_rank=0
     
-    def compute_matrix_list(self):
-                d_matrix_list=[]
-                for i in range(len(self.experts)):
-                    d_matrix=self.experts[i](self.x)
-                    d_matrix=d_matrix.detach()
-                    d_matrix_list.append(d_matrix)
-
-                #init weight matrix, numerical
-                def diag_weight(size):
-                    #梯形公式
-                    weights = torch.ones(size)
-                    weights[0] = weights[-1] = 0.5
-                    diag_matrix = torch.diag(weights).to(self.device)
-                    return diag_matrix
-                weight_matrix=diag_weight(self.num_samples)
-                #D.T W D
-                M_weight_list=[]
-                for i in range(len(d_matrix_list)):
-                    M_weight = torch.matmul(d_matrix_list[i].t(), weight_matrix)
-                    M_weight = torch.matmul(M_weight, d_matrix_list[i])
-                    M_weight_list.append(M_weight)
-                # y_np = d_matrix.cpu().numpy()
-                # print("moe ouput:", y_np.shape)
-                return  M_weight_list
-    
-    def compute_total_matrix(self):
+    def compute_experts_matrix_list(self):
         d_matrix_list=[]
-        for i in range(len(self.experts)):
-                    d_matrix=self.experts[i](self.x)
-                    d_matrix=d_matrix.detach()
-                    d_matrix_list.append(d_matrix)
-        D_matrix=torch.cat(d_matrix_list, dim=1)
+        for i in range(self.model.num_experts):
+           
+            partial_model=PartialExpert(self.model,i, self.activation)
+            d_matrix=partial_model(self.X_total)
+            d_matrix=d_matrix.detach()
+            d_matrix_list.append(d_matrix)
+
+        big_matrix = torch.cat(d_matrix_list, dim=1)
+        d_matrix_list.append(big_matrix)
+
+
         #init weight matrix, numerical
-        def diag_weight(size):
+        def diag_weight(x_size, t_size):
             #梯形公式
-            weights = torch.ones(size)
-            weights[0] = weights[-1] = 0.5
-            diag_matrix = torch.diag(weights).to(self.device)
+            weights = torch.ones((x_size, t_size))
+            weights[0, :] *= 0.5
+            weights[-1, :] *= 0.5
+            weights[:, 0] *= 0.5
+            weights[:, -1] *= 0.5
+            weights = weights.flatten()
+            diag_matrix = (self.x[-1]-self.x[0])/(x_size-1) * (self.t[-1]-self.t[0])/(t_size-1) * torch.diag(weights).to(self.device)
             return diag_matrix
-        weight_matrix=diag_weight(self.num_samples)
+        
+        weight_matrix=diag_weight(self.x_num_samples, self.t_num_samples)
         #D.T W D
-        M_weight=0
-        M_weight = torch.matmul(D_matrix.t(), weight_matrix)
-        M_weight = torch.matmul(M_weight, D_matrix)
+        M_weight_list_experts=[]
+        for i in range(len(d_matrix_list)):
+            M_weight = torch.matmul(d_matrix_list[i].t(), weight_matrix)
+            M_weight = torch.matmul(M_weight, d_matrix_list[i])
+            M_weight_list_experts.append(M_weight)
+        self.M_weight_list_experts = M_weight_list_experts
         # y_np = d_matrix.cpu().numpy()
         # print("moe ouput:", y_np.shape)
-        return  M_weight
+        return  M_weight_list_experts
         
-        
-    def rank_mlp(self,total_gram=False):
-        "如果要计算total rank 那么输入total_gram改为true"
-        if total_gram:
-            gram=self.M_weight_total
-        else:
-            gram=self.M_weight_list
-        
-        if isinstance(gram, torch.Tensor):
-            gram = [gram]
-        rank_list=[]
-        for i in range(len(gram)):
-            eigvals = torch.linalg.eigvalsh(gram[i])
-
-            # 设定阈值 epsilon
-            epsilon = 1e-3
+    def experts_rank_mlp(self):
+        rank_list_experts=[]
+        for i in range(len(self.M_weight_list_experts)):
+            eigvals = torch.linalg.eigvalsh(self.M_weight_list_experts[i])
 
             # 统计大于 epsilon 的特征值数量
-            count = (eigvals > epsilon).sum().item()
-            rank_list.append(count)
-        self.rank_list = rank_list
-        return rank_list
-    
+            count = (eigvals > self.epsilon).sum().item()
+            rank_list_experts.append(count)
+        rank_list_experts.append(sum(rank_list_experts[:-1]) - rank_list_experts[-1])
+        self.rank_list_experts = rank_list_experts
+        return rank_list_experts
     
 class PartialMOE(nn.Module):
-    def __init__(self, model, n,index=1):
+    def __init__(self, model, n,index=0):
         super().__init__()
         self.model = model
-        self.n = n # partial model length
-        self.index=index #default 1   为了第一个保证【：1】 当n=0时能取到第一层的输出
+        self.n = n
+        self.index=index
 
-    def forward(self, x,training,moe_training=True):
+    def forward(self, x,training,moe_traing=True):
         for i, layer in enumerate(self.model.model[:self.n+self.index]):
-            if i == 0 and moe_training:
+            if i == 1 and moe_traing:
                 x, _ = layer(x,training)
             else:
                 x = layer(x)
         return x
     
-    
-class Expert_output(nn.Module):
-    def __init__(self, model,n,index):
-        super().__init__()
-        self.model=model
-        self.n=n # expert index
-        self.index=index # moe layer index moe层的位置，默认model里只包含一个moe层，其余均为fcnn
-        self.expert=self.model.model.experts[self.n]
-        # print("expert output:",self.expert)
-    
-    def forward(self,x):
-        x=self.expert(x)
-        return x
 
-class flatten_rank():
-    def __init__(self, model,interval,num_samples):
-        """
-        This is a class for computing the rank of a flatten model. --1D
-        Parameters:
-        model: class MOE_Model in moe.py
-        interval: the interval of the input"
-        num_samples: the number of samples
-        
-        """
-        self.model=model
-        self.moe=self.model.moe
-        self.experts=self.moe.experts # modeul.experts 是一个list
-        self.gates=self.moe.gating_network
-        self.flatten_moe=flatten_moe(model,self.moe.k)
-        interval = eval(interval)  # 例如 "[-1,1]" -> [-1, 1]
-        self.device=next(model.parameters()).device
-        self.x = torch.linspace(interval[0], interval[1], num_samples).view(-1, 1).to(self.device)
-        self.num_samples = num_samples
-        self.M_weight=self.compute_matrix()
-        self.rank=0
-        
-    
-    def compute_matrix(self):
-                d_matrix=self.flatten_moe(self.x)
-                d_matrix=d_matrix.detach()
-
-                #init weight matrix, numerical
-                def diag_weight(size):
-                    #梯形公式
-                    weights = torch.ones(size)
-                    weights[0] = weights[-1] = 0.5
-                    diag_matrix = torch.diag(weights).to(self.device)
-                    return diag_matrix
-                weight_matrix=diag_weight(self.num_samples)
-                #D.T W D
-                M_weight = torch.matmul(d_matrix.t(), weight_matrix)
-                M_weight = torch.matmul(M_weight, d_matrix)
-                # y_np = d_matrix.cpu().numpy()
-                # print("moe ouput:", y_np.shape)
-                return  M_weight
-    
-        
-    def rank_mlp(self):
-        gram=self.M_weight
-        
-        eigvals = torch.linalg.eigvalsh(gram)
-
-        # 设定阈值 epsilon
-        epsilon = 1e-3
-
-        # 统计大于 epsilon 的特征值数量
-        count = (eigvals > epsilon).sum().item()
-        self.rank = count
-        return count
-    
-     
-class flatten_moe(nn.Module):
-    def __init__(self, model,k):
+class PartialExpert(nn.Module):
+    def __init__(self, model, n, activation):
         super().__init__()
         self.model = model
-        self.moe=self.model.moe
-        self.k=k
-        self.experts=self.model.model.experts # modeul.experts 是一个list
-        self.gates=self.moe.gating_network
-        self.softmax = nn.Softmax(dim=-1)
+        self.n = n
+        self.activation=activation
+        # self.layer1=self.model.model[0]
+        self.expert=self.model.model[0].experts[n]
 
     def forward(self, x):
         
-# 获取 topk 的索引
-        noisy,_,_=self.gates(x,False)
-        values, indices= torch.topk(noisy,k=self.k,dim=1) 
-        values=self.softmax(values)
-        zeros= torch.zeros_like(noisy, requires_grad=True)
-        gates=zeros.scatter(1, indices, values)
-        y=torch.stack([gates[:,i].view(-1, 1)*self.experts[i](x) for i in range(len(self.experts))])
-        y = y.permute(1, 0, 2)
-        y= y.reshape(y.shape[0],-1)
-        return y
+        # x = self.layer1(x)
+        for i, layer in enumerate(self.expert.net[:-1]):
+            x = layer(x)
+            x = self.activation(x)
+
+        return x
--- moe_module/utils.py
+++ moe_module/utils.py
@@ -11,37 +11,43 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Train a Mixture-of-Experts model.")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs.")
     parser.add_argument("--lr", type=float, default=1e-2, help="Learning rate.")
-    parser.add_argument("--device", type=str, default="cuda:3" if torch.cuda.is_available() else "cpu", help="Device to train on.")    
-    parser.add_argument("--input_size", type=int, default=1,help="Input size (funcion approximation x) ")
+    parser.add_argument("--device", type=str, default="cuda:5" if torch.cuda.is_available() else "cpu", help="Device to train on.")    
+    parser.add_argument("--input_size", type=int, default=2,help="Input size (funcion approximation x) ")
     parser.add_argument("--output_size", type=int, default=1,help="Output size (funcion approximation y=u(x)) ")
-    parser.add_argument("--num_experts", type=int, default=3,help="Number of experts")
-    parser.add_argument("--hidden_size", type=int, default=50,help="Hidden size of the MLP")
-    parser.add_argument("--depth", type=int, default=2,help="Depth of the MOE model")
+    parser.add_argument("--num_experts", type=int, default=5,help="Number of experts")
+    parser.add_argument("--hidden_size", type=int, default=10,help="Hidden size of each expert")
+    parser.add_argument("--depth", type=int, default=3,help="Depth of the MOE model")
     parser.add_argument("--lossfn", type=str, default="mse", help="Loss function.")
-    parser.add_argument("--optim", type=str, default="adam")
-    parser.add_argument("--opt_steps", type=int, default=10000)
-    parser.add_argument("--function", type=str, default="cos5x+sin100x+cos30x", help="function")
-    parser.add_argument("--interval", type=str, default="[-1,1]")
-    parser.add_argument("--num_samples", type=int, default=1000)
+    parser.add_argument("--optim", type=str, default="adamw")
+    parser.add_argument("--opt_steps", type=int, default=20000)
+    parser.add_argument("--activation", type=str, default="tanh", help="activation_function")
+    parser.add_argument("--init_func", type=str, default="-sin(pi*x)", help="function")
+    parser.add_argument("--x_interval", type=str, default="[-1,1]")
+    parser.add_argument("--t_interval", type=str, default="[0,1]")
+    parser.add_argument("--x_num_samples", type=int, default=400)
+    parser.add_argument("--t_num_samples", type=int, default=200)
     parser.add_argument("--k", type=int, default=2,help="top-k selection")
     parser.add_argument("--loss_coef", type=float, default=1)
-    parser.add_argument("--integral_sample", type=int, default=300, help="integral_sample")
+    parser.add_argument("--x_integral_sample", type=int, default=200, help="x_integral_sample")
+    parser.add_argument("--t_integral_sample", type=int, default=100, help="t_integral_sample")
+    parser.add_argument("--nu", type=float, default=0.01,help="nu in equation u_t + u*u_x - nu*u_xx=0")
     parser.add_argument("--plt_r", type=int, default=1)
-    parser.add_argument("--decrease_rate", type=float, default=0.7)
-    parser.add_argument("--index", type=int, default=0,help="index of the expert")
+    parser.add_argument("--epsilon", type=float, default=1e-3)
+    parser.add_argument("--loss_coef_init", type=float, default=1)
+    parser.add_argument("--loss_coef_bnd", type=float, default=1)
     return parser.parse_args()
-def _init_data_dim1(func: str, interval: str, num_samples: int,device):
+def _init_data_dim1(func: str, x_interval: str, t_interval: str, x_num_samples: int, t_num_samples: int, device):
     """
     初始化数据 
     """
-    interval = eval(interval)  # 例如 "[-1,1]" -> [-1, 1]
+    x_interval = eval(x_interval)  # 例如 "[-1,1]" -> [-1, 1]
+    t_interval = eval(t_interval)
     # x = (interval[1] - interval[0]) * torch.rand(num_samples, 1) + interval[0]
-    x=torch.linspace(interval[0], interval[1], num_samples).view(-1, 1)
+    x=torch.linspace(x_interval[0], x_interval[1], x_num_samples).view(-1, 1)
+    t=torch.linspace(t_interval[0], t_interval[1], t_num_samples).view(-1, 1)
     # 定义函数解析器
     def parse_function(expr: str):
-        expr = expr.replace("cos30x", "np.cos(30*x_np)")
-        expr = expr.replace("sin100x", "np.sin(100*x_np)")
-        expr = expr.replace("cos5x", "np.cos(5*x_np)")
+        expr = expr.replace("-sin(pi*x)", "-np.sin(np.pi*x_np)")
         def f(x_tensor):
             x_np = x_tensor.numpy()
             y_np = eval(expr)
@@ -49,15 +55,35 @@ def _init_data_dim1(func: str, interval: str, num_samples: int,device):
         return f
 
     f = parse_function(func)
-    y = f(x)
-    x = x.to(device)
-    y = y.to(device)
-    return x, y
+    u_init = f(x)
+
+    X, T = torch.meshgrid(x.squeeze(), t[0].squeeze(), indexing='ij')
+    X_init = torch.stack([X.flatten(), T.flatten()], dim=1)
+    X_init = X_init.to(device)
+
+    X, T = torch.meshgrid(x[[0,-1]].squeeze(), t[1:].squeeze(), indexing='ij')
+    X_bnd = torch.stack([X.flatten(), T.flatten()], dim=1)
+    X_bnd = X_bnd.to(device)
+
+    X, T = torch.meshgrid(x[1:-1].squeeze(), t[1:].squeeze(), indexing='ij')
+    X_f = torch.stack([X.flatten(), T.flatten()], dim=1)
+    X_f = X_f.to(device)
+
+    X, T = torch.meshgrid(x.squeeze(), t.squeeze(), indexing='ij')
+    X_total = torch.stack([X.flatten(), T.flatten()], dim=1)
+    X_total = X_total.to(device)
+
+    X_init = X_init.to(device)
+    X_bnd = X_bnd.to(device)
+    X_f = X_f.to(device)
+    X_total = X_total.to(device)
+    u_init = u_init.to(device)
+    return X_init, X_bnd, X_f, X_total, u_init
 
 def get_optimizer(name, model_params, lr=1e-3):
     name = name.lower()
     if name == "adam":
-        return optim.Adam(model_params,lr=lr,   betas=(0.9, 0.999), eps=1e-9)
+        return optim.Adam(model_params,lr=lr,   betas=(0.9, 0.98), eps=1e-9,weight_decay=1e-4)
     elif name == "sgd":
         return optim.SGD(model_params, lr=lr, momentum=0.9)
     elif name == "adamw":
@@ -113,7 +139,7 @@ def plot_dual_axis(loss: np.ndarray, rank: np.ndarray, step: int,name):
     ax1.plot(z, loss, 'b-', label='Loss')
     ax1.set_xlabel('Iterations')
     ax1.set_ylabel('Loss', color='b')
-    ax1.set_yscale('log', base=10) 
+    ax1.set_yscale('log', base=10)
     ax1.tick_params(axis='y', labelcolor='b')
 
     ax2 = ax1.twinx()  # 创建共享 x 轴的第二个 y 轴
@@ -124,6 +150,35 @@ def plot_dual_axis(loss: np.ndarray, rank: np.ndarray, step: int,name):
     fig.tight_layout()
     plt.title("Loss vs. Rank")
     plt.savefig(f"loss_vs_rank_{name}.png")  # 保存图像
+    plt.show()
+
+def plot_expert_useless_rank(loss: np.ndarray, rank: np.ndarray, step: int,name):
+    z = np.arange(1, step + 1)
+
+    # 插值处理 rank
+    if len(rank) != step:
+        from scipy.interpolate import interp1d
+        interp = interp1d(np.linspace(0, step - 1, len(rank)), rank, kind='linear', fill_value="extrapolate")
+        rank_interp = interp(np.arange(step))
+    else:
+        rank_interp = rank
+
+    fig, ax1 = plt.subplots(figsize=(17, 9))
+
+    ax1.plot(z, loss, 'b-', label='Loss')
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Loss', color='b')
+    ax1.set_yscale('log', base=10)
+    ax1.tick_params(axis='y', labelcolor='b')
+
+    ax2 = ax1.twinx()  # 创建共享 x 轴的第二个 y 轴
+    ax2.plot(z, rank_interp, 'r--', label='Useless Rank')
+    ax2.set_ylabel('Useless Rank', color='r')
+    ax2.tick_params(axis='y', labelcolor='r')
+
+    fig.tight_layout()
+    plt.title("Loss vs. Useless Rank")
+    plt.savefig(f"loss_vs_useless_rank_{name}.png")  # 保存图像
     plt.show()
 
 def get_activation(name: str):
@@ -138,4 +193,3 @@ def get_activation(name: str):
         return nn.GELU()
     else:
         raise ValueError(f"Unsupported activation: {name}")
-
