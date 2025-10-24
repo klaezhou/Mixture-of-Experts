b17cee2063ddadc133155217f9a21ef92b6d0633
fengyin798 <352835621@qq.com>
2025-10-24 13:47:18 +0000

    新增真解图像，新增学习率下降，新增TensorBoard保存训练图像记录，修改随机撒点训练。

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ from torch import nn
 import torch.optim as optim
 import argparse
 import numpy as np
+from pathlib import Path
 from functorch import make_functional, vmap
 from moe_module.moe import MOE_Model,MLP_Model
-from moe_module.utils import parse_args, _init_data_dim1, get_optimizer, get_loss_fn, log_with_time,plot_dual_axis, plot_expert_useless_rank, get_activation
+from moe_module.utils import parse_args, _init_data_dim1, get_optimizer, get_loss_fn, log_with_time,plot_dual_axis, plot_expert_useless_rank, get_activation, generate_data
 from moe_module.epi_rank import epi_rank_mlp
+from moe_module.tools import *
 # download data
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from torch.autograd import grad
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 torch.set_default_dtype(torch.float64)
 
@@ -40,8 +43,20 @@ def pde_residual(model, x_t, nu, moe_training=True):
     r = u_t + u * u_x - nu * u_xx
     return r
 
+def ground_truth(args, writer):
+    """
+    u: model output
+    x_t: tensor shape (N,2) with columns [x, t], requires_grad=True
+    returns: residual r = u_t + u u_x - nu u_xx
+    """
+    gt,X_test=generate_data(args.vxn,args.vtn,args.nu, writer)
+    X_test=X_test.to(args.device)
+    gt=gt.to(args.device)
+    args.gt=gt
+    args.X_test=X_test
+
 @log_with_time
-def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,steps=100,moe_training=True):
+def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, scheduler, args,steps=100,moe_training=True, writer=None):
     activation=get_activation(args.activation)
     aux_loss=0
     init_loss=0
@@ -50,10 +65,12 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
     total_loss_list=[]
     total_rank_list=[]
     total_useless_expert_rank=[]
-    for step in range(steps):
+    tqdm_range=tqdm(range(steps), desc="Training Progress")
+
+    for step in tqdm_range:
         model.train()
         if step % 1000 == 0:
-            eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training,args)
+            eval_model(step,X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training,args, writer)
         # 确保在训练模式
         if moe_training:
             u_hat_init, _ = model(X_init)
@@ -79,6 +96,13 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
         optim.zero_grad()
         total_loss.backward()
         optim.step()
+        scheduler.step()
+
+        if moe_training:
+            tqdm_range.set_postfix({'loss': f'{total_loss.item():.8f}', 'aux_loss': f'{aux_loss.item():.8f}'})
+        else:
+            tqdm_range.set_postfix({'loss': f'{total_loss.item():.8f}'})
+
         if step % 100 == 0 or step == steps - 1:
             if moe_training:
                 # rank_moe=epi_rank_moe(model,args.interval,args.integral_sample)
@@ -88,16 +112,21 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
                 rank_list=rank_mlp.rank_mlp()
                 total_rank_list.append(rank_list[0])
                 rank_list_experts=rank_mlp.experts_rank_mlp()
-                print(f"Step {step+1}/{steps} - loss: {loss.item():.8f} -aux_loss: {aux_loss.item():.8f} -rank: {rank_list[0]},{rank_list} -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
+                tqdm.write(f"Step {step+1}/{steps} - loss: {loss.item():.8f} -aux_loss: {aux_loss.item():.8f} -rank: {rank_list[0]},{rank_list} -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
+                writer.add_scalar('MoE_Loss', total_loss.item(), step)
+                writer.add_scalar('Aux_Loss', aux_loss.item(), step)
+                writer.add_scalar('MoE_Rank', rank_list[0], step)
+                writer.add_scalar('Useless_Expert_Rank', rank_list_experts[-1], step)
                 total_useless_expert_rank.append(rank_list_experts[-1])
             else:
                 rank_mlp=epi_rank_mlp(model,args.x_interval,args.t_interval,args.x_integral_sample, args.t_integral_sample,args.epsilon, activation ,moe_training=False,index=1)
                 rank=rank_mlp.rank_mlp()
                 total_rank_list.append(rank[args.plt_r])
-                print(f"Step {step+1}/{steps} - loss: {loss.item():.8f} -rank: {rank}")
+                tqdm.write(f"Step {step+1}/{steps} - loss: {loss.item():.8f} -rank: {rank}")
+                writer.add_scalar('MLP_Loss', total_loss.item(), step)
     return model,total_loss_list,total_rank_list,total_useless_expert_rank
             
-def eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=True,args=None):
+def eval_model(step,X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=True,args=None, writer=None):
     model.eval()
     # model returns the prediction and the loss that encourages all experts to have equal importance and load
     if moe_training:
@@ -116,14 +145,19 @@ def eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=
     loss = args.loss_coef_init *init_loss + args.loss_coef_bnd * bnd_loss + f_loss
 
     if moe_training:
-        print("MoE_Model Evaluation Results - loss: {:.8f}, aux_loss: {:.8f}".format(loss.item(), aux_loss.item()))
+        tqdm.write("MoE_Model Evaluation Results - loss: {:.8f}, aux_loss: {:.8f}".format(loss.item(), aux_loss.item()))
     else:
-        print("MLP_Model Evaluation Results - loss: {:.8f}".format(loss.item()))
+        tqdm.write("MLP_Model Evaluation Results - loss: {:.8f}".format(loss.item()))
 
 
-    x = X_total[:, 0].detach().cpu().numpy()
-    t = X_total[:, 1].detach().cpu().numpy()
-    u_total = u_total.detach().cpu().numpy().flatten()
+    # 1---------- 绘制预测----------
+    if moe_training:
+        u_pred,_= model(args.X_test)
+    else:
+        u_pred = model(args.X_test)
+    x = args.X_test[:, 0].detach().cpu().numpy()
+    t = args.X_test[:, 1].detach().cpu().numpy()
+    u_pred = u_pred.detach().cpu().numpy().flatten()
 
     # 尝试重建网格（假设 x 是 meshgrid flatten 后的）
     nx = len(np.unique(x))
@@ -131,12 +165,11 @@ def eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=
     X = np.unique(x)
     T = np.unique(t)
     X, T = np.meshgrid(X, T, indexing='ij')
-    u_total = u_total.reshape(nx, nt)
-
-    # ---------- 绘制预测 vs 真值 ----------
+    u_pred = u_pred.reshape(nx, nt)
+    
     fig, axs = plt.subplots(figsize=(17, 9))
 
-    im0 = axs.contourf(X, T, u_total, levels=100, cmap='coolwarm', vmin=-1, vmax=1)
+    im0 = axs.contourf(X, T, u_pred, levels=100, cmap='coolwarm', vmin=-1, vmax=1)
     axs.set_title("Solution_pred")
     axs.set_xlabel("x")
     axs.set_ylabel("t")
@@ -145,8 +178,56 @@ def eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=
     fig.tight_layout()
     if moe_training:
         plt.savefig("solution_pred_moe.png")
+        writer.add_figure("Solution_pred_MoE", fig, step)
     else:
         plt.savefig("solution_pred_mlp.png")
+        writer.add_figure("Solution_pred_MLP", fig, step)
+    plt.show()
+    
+    #  2----------绘制误差-----------
+    if moe_training:
+        u_test,_= model(args.X_test)
+        u_error = torch.abs(u_test - args.gt)
+    else:
+        u_error = torch.abs(model(args.X_test) - args.gt)
+    rell2=torch.norm(u_error, p=2) / torch.norm(args.gt, p=2)
+    tqdm.write(f"L2 absolute error: {rell2.item():.8f}")
+    if moe_training:
+        # np.save("l2_moe.npy", np.array(rell2.detach().cpu().numpy()))
+        writer.add_scalar('MoE_L2_Error', rell2.item(), step)
+    else:
+        # np.save("l2_mlp.npy", np.array(rell2.detach().cpu().numpy()))
+        writer.add_scalar('MLP_L2_Error', rell2.item(), step)
+    
+
+    
+    x = args.X_test[:, 0].detach().cpu().numpy()
+    t = args.X_test[:, 1].detach().cpu().numpy()
+    u_error = u_error.detach().cpu().numpy().flatten()
+
+    # 尝试重建网格（假设 x 是 meshgrid flatten 后的）
+    nx = len(np.unique(x))
+    nt = len(np.unique(t))
+    X = np.unique(x)
+    T = np.unique(t)
+    X, T = np.meshgrid(X, T, indexing='ij')
+    u_error = u_error.reshape(nx, nt)
+    
+    fig2, ax2 = plt.subplots(figsize=(10, 6))
+
+    im2 = ax2.contourf(X, T, u_error, levels=100, cmap='viridis')
+    ax2.set_title("Absolute Error")
+    ax2.set_xlabel("x")   
+    ax2.set_ylabel("t")
+    fig2.colorbar(im2, ax=ax2)
+
+    fig2.tight_layout()
+    if moe_training:
+        plt.savefig("abe_moe.png")
+        writer.add_figure("Absolute_Error_MoE", fig2, step)
+    else:
+        plt.savefig("abe_mlp.png")
+        writer.add_figure("Absolute_Error_MLP", fig2, step)
     plt.show()
 
 # %%
@@ -182,6 +263,16 @@ def eval_model(X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training=
 
 def main():
     args=parse_args()
+
+    logs_root = Path(get_logs_root())
+    log_dir = logs_root / f"{runcode}-{Timetxt}"
+    log_dir.mkdir(parents=True, exist_ok=True)
+    writer = create_summary_writer(log_dir)
+    writer.add_text("config", str(args))
+    os.makedirs("figs", exist_ok=True)
+    os.makedirs("figs/im2", exist_ok=True)
+
+    ground_truth(args, writer)
     #init data and model
     data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init=_init_data_dim1(args.init_func,args.x_interval,args.t_interval,args.x_num_samples,args.t_num_samples,args.device)
     print(f"data_X_init shape: {data_X_init.shape},data_X_bnd shape: {data_X_bnd.shape},data_X_f shape: {data_X_f.shape},data_X_total shape: {data_X_total.shape},data_u_init shape: {data_u_init.shape} ")
@@ -189,22 +280,23 @@ def main():
     model_moe=MOE_Model(args.input_size, args.num_experts,args.hidden_size,args.depth, args.output_size,args.k,args.loss_coef, activation).to(args.device)
     loss_fn =get_loss_fn(args.lossfn)
     optimizer = get_optimizer(args.optim,model_moe.parameters(), lr=args.lr)
-    model,total_loss_list_moe,rank_list_moe,total_useless_expert_rank_moe=train_loop(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_moe,loss_fn, optimizer, args,args.opt_steps)
-    eval_model(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model, loss_fn,moe_training=True,args=args)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step, gamma=args.lr_decay)
+    model,total_loss_list_moe,rank_list_moe,total_useless_expert_rank_moe=train_loop(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_moe,loss_fn, optimizer, scheduler, args,args.opt_steps, moe_training=True, writer=writer)
+    eval_model(args.opt_steps,data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model, loss_fn,moe_training=True,args=args, writer=writer)
     torch.set_printoptions(threshold=float('inf'))
     # print("Gates:\n", model.model[1].gates_check)
     model_mlp=MLP_Model(args.input_size, args.hidden_size*args.num_experts,args.depth, args.output_size, activation).to(args.device)
     optimizer_mlp=get_optimizer(args.optim,model_mlp.parameters(), lr=args.lr)
-        
-    model_mlp,total_loss_list_mlp,rank_list_mlp,total_useless_expert_rank_mlp=train_loop(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_mlp,loss_fn, optimizer_mlp, args,args.opt_steps,moe_training=False)
-    eval_model(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_mlp, loss_fn,moe_training=False,args=args)
+    scheduler_mlp = torch.optim.lr_scheduler.StepLR(optimizer_mlp, step_size=args.lr_step, gamma=args.lr_decay)
+    model_mlp,total_loss_list_mlp,rank_list_mlp,total_useless_expert_rank_mlp=train_loop(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_mlp,loss_fn, optimizer_mlp, scheduler_mlp, args,args.opt_steps,moe_training=False, writer=writer)
+    eval_model(args.opt_steps,data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model_mlp, loss_fn,moe_training=False,args=args, writer=writer)
     
     
-    plot_dual_axis(np.array(total_loss_list_moe),np.array(rank_list_moe),args.opt_steps,"moe")
-    plot_dual_axis(np.array(total_loss_list_mlp),np.array(rank_list_mlp),args.opt_steps,"mlp")
+    plot_dual_axis(np.array(total_loss_list_moe),np.array(rank_list_moe),args.opt_steps,"moe",writer)
+    plot_dual_axis(np.array(total_loss_list_mlp),np.array(rank_list_mlp),args.opt_steps,"mlp",writer)
     plot_expert_useless_rank(np.array(total_loss_list_moe),np.array(total_useless_expert_rank_moe),args.opt_steps,"moe")
 
 if __name__ == "__main__":
-    torch.manual_seed(1234)
+    torch.manual_seed(42)
     main()      
     
