sftp softmax+cross train + fix tau

_fun_run/_soft_1D.py
@@ -27,28 +27,28 @@ torch.set_default_dtype(torch.float64)
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a Mixture-of-Experts model.")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs.")
-    parser.add_argument("--lr", type=float, default=2e-2, help="Learning rate.")
+    parser.add_argument("--lr", type=float, default=5e-3, help="Learning rate.")
     parser.add_argument("--device", type=str, default="cuda:3" if torch.cuda.is_available() else "cpu", help="Device to train on.")    
     parser.add_argument("--input_size", type=int, default=1,help="Input size (funcion approximation x) ")
     parser.add_argument("--output_size", type=int, default=1,help="Output size (funcion approximation y=u(x)) ")
     parser.add_argument("--num_experts", type=int, default=2,help="Number of experts")
-    parser.add_argument("--hidden_size", type=int, default=8,help="Hidden size of the MLP")
-    parser.add_argument("--depth", type=int, default=2,help="Depth of the MOE model")
+    parser.add_argument("--hidden_size", type=int, default=5,help="Hidden size of the MLP")
+    parser.add_argument("--depth", type=int, default=3,help="Depth of the MOE model")
     parser.add_argument("--lossfn", type=str, default="mse", help="Loss function.")
     parser.add_argument("--optim", type=str, default="adam")
     parser.add_argument("--opt_steps", type=int, default=6000)
-    parser.add_argument("--opt_steps_gate", type=int, default=1000)
+    parser.add_argument("--opt_steps_gate", type=int, default=15000)
     parser.add_argument("--function", type=str, default="func2", help="function")
     parser.add_argument("--interval", type=str, default="[-1,1]")
-    parser.add_argument("--num_samples", type=int, default=1000)
+    parser.add_argument("--num_samples", type=int, default=2000)
     parser.add_argument("--k", type=int, default=1,help="top-k selection")
-    parser.add_argument("--loss_coef", type=float, default=0.1)
+    parser.add_argument("--loss_coef", type=float, default=0)
     parser.add_argument("--integral_sample", type=int, default=300, help="integral_sample")
     parser.add_argument("--plt_r", type=int, default=1)
     parser.add_argument("--decrease_rate", type=float, default=0.9)
     parser.add_argument("--index", type=int, default=0,help="index of the expert")
-    parser.add_argument("--smooth_steps",type=int,default=2,help="number of steps for smooth mode")
-    parser.add_argument("--smooth_lb",type=int,default=900,help="number lower bound of steps for smooth mode")
+    parser.add_argument("--smooth_steps",type=int,default=12,help="number of steps for smooth mode")
+    parser.add_argument("--smooth_lb",type=int,default=10000,help="number lower bound of steps for smooth mode")
     parser.add_argument("--seed",type=int,default=123) #1234
     parser.add_argument("--ep",type=torch.tensor,default=torch.tensor([[5e-3]],device="cuda:3" if torch.cuda.is_available() else "cpu")) #1234
     parser.add_argument("--ep1",type=torch.tensor,default=torch.tensor([[-5e-3]],device="cuda:3" if torch.cuda.is_available() else "cpu")) #1234
@@ -149,14 +149,14 @@ def train_loop(x, y, model,loss_fn, optim, args,steps=100,moe_training=True):
     for step in range(steps):
         
         model.train()
-        # if moe_training :
-        #     step_count -=1
-        #     if model.moe.smooth and step_count<=0:
-        #         model.moe.smoothing(step,args.smooth_lb)
-        #         step_count=args.smooth_steps
-        #     elif step_count<=0 :
-        #         model.moe.smoothing(step,args.smooth_lb)
-        #         step_count=args.smooth_steps
+        if moe_training :
+            step_count -=1
+            if model.moe.smooth and step_count<=0:
+                model.moe.smoothing(step,args.smooth_lb)
+                step_count=args.smooth_steps
+            elif step_count<=0 :
+                model.moe.smoothing(step,args.smooth_lb)
+                step_count=args.smooth_steps
                 
         if moe_training:
             y_hat, aux_loss = model(x)
@@ -164,18 +164,25 @@ def train_loop(x, y, model,loss_fn, optim, args,steps=100,moe_training=True):
             y_hat=model(x)
         # calculate prediction loss
         loss = loss_fn(y_hat, y)
-        # combine losses
+        # combine lossesss
         if moe_training:
-            total_loss = loss + aux_loss
+            lambda_l2 = 0#1e-6
+            # reg = lambda_l2 * (((model.moe.tau1) ** 2).sum() + ((model.moe.tau2 )** 2).sum())
+            reg=lambda_l2 *(((model.moe.tau1*model.moe.tau2))).sum()
+            if model.moe.smooth:
+                total_loss = loss + aux_loss
+            else: total_loss = loss + aux_loss
         else:
             total_loss = loss
         total_loss_list.append(total_loss.item())
-        optim.zero_grad()
-        total_loss.backward()
+        optim.zero_grad() 
+        total_loss.backward() 
         optim.step()
         scheduler.step()
-        if step % 10 == 0 or step == steps - 1:
+        if step % 300 == 0 or step == steps - 1:
             print(f"Step {step+1}/{steps} - loss: {loss.item():.8f}")
+            print("model.moe.tau1:",model.moe.tau1.detach().cpu().numpy())
+            print("model.moe.tau2:",model.moe.tau2.detach().cpu().numpy())
             eval_model(args.data_x, args.data_y, model, loss_fn,moe_training,args)
     return model,total_loss_list
             
@@ -229,8 +236,9 @@ def eval_model(x, y, model, loss_fn,moe_training=True,args=None):
         else:
             gates,load= model.model.topkGating(x,False)
 
-        ax2.plot(x.cpu().numpy(), gates[:,0].detach().cpu().numpy(), label=f'gates 0')
-        ax2.plot(x.cpu().numpy(), gates[:,1].detach().cpu().numpy(), label=f'gates 1')
+
+        for i in range(model.model.num_experts):
+            ax2.plot(x.cpu().numpy(), gates[:,i].detach().cpu().numpy(), label=f'gates {i}')
         plt.legend()
 
         fig.tight_layout()
@@ -250,16 +258,16 @@ def main():
     #%%
     # pre_train(data_x, data_y, loss_fn, args)
     args.data_x, args.data_y=data_x, data_y
-    train_index = (data_x[:, 0] < -0.25) | (data_x[:, 0] > 0.25)
+    train_index = (data_x[:, 0] <= 0) | (data_x[:, 0] > 0)
     data_x_test=data_x[train_index,:]
     data_y_test=data_y[train_index,:]
     model_moe=MOE_modify_betap(args.input_size, args.num_experts,args.hidden_size,args.depth+1, args.output_size,args.k,args.loss_coef).to(args.device)
-    load_model(args, model_moe)
+    # load_model(args, model_moe)
     
-    for p in model_moe.model.experts.parameters():
-        p.requires_grad = False
-    for p in model_moe.model.gating_network.parameters():
-        p.requires_grad = True
+    # for p in model_moe.model.experts.parameters():
+    #     p.requires_grad = False
+    # for p in model_moe.model.gating_network.parameters():
+    #     p.requires_grad = True
 
     optimizer = get_optimizer(args.optim,model_moe.parameters(), lr=args.lr)
     model,total_loss_list_moe=train_loop(data_x_test, data_y_test, model_moe,loss_fn, optimizer, args,args.opt_steps_gate)

_fun_run/main.py
@@ -89,9 +89,11 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
         bnd_loss = loss_fn(u_hat_bnd, torch.zeros_like(u_hat_bnd))  # zero boundary condition
         f_residual = pde_residual(model, X_f, args.nu, moe_training)
         f_loss = loss_fn(f_residual, torch.zeros_like(f_residual))
-        loss = args.loss_coef_init *init_loss + args.loss_coef_bnd * bnd_loss + f_loss
+        lambda_l2 = 1e-4 #1e-6
+        reg = lambda_l2 * (((1/model.moe.tau1) ** 2).sum() + ((1/model.moe.tau2 )** 2).sum())
+        loss = args.loss_coef_init *init_loss + args.loss_coef_bnd * bnd_loss + f_loss+reg
 
-        # combine losses
+        # combine loss
         if moe_training:total_loss = loss + aux_loss
         else: total_loss = loss
         total_loss_list.append(total_loss.item())
@@ -103,17 +105,18 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
                 # rank_moe=epi_rank_moe(model,args.interval,args.integral_sample)
                 # rank=rank_moe.rank_moe()
                 
-                rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample, args.epsilon,moe_training=True,index=1)
-                rank_list=rank_mlp.rank_mlp()
-                total_rank_list.append(rank_list[args.plt_r])
-                rank_list_experts=rank_mlp.experts_rank_mlp()
-                tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss.item():.8f} -aux_loss: {aux_loss.item():.8f} -rank: {rank_list} \
-                    -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
+                # rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample, args.epsilon,moe_training=True,index=1)
+                # rank_list=rank_mlp.rank_mlp()
+                # total_rank_list.append(rank_list[args.plt_r])
+                # rank_list_experts=rank_mlp.experts_rank_mlp()
+                # tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss.item():.8f} -aux_loss: {aux_loss.item():.8f} -rank: {rank_list} \
+                #     -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
                 writer.add_scalar('MoE_Loss', total_loss.item(), step)
                 writer.add_scalar('Aux_Loss', aux_loss.item(), step)
-                writer.add_scalar('MoE_Rank', rank_list[args.plt_r], step)
-                writer.add_scalar('Useless_Expert_Rank', rank_list_experts[-1], step)
-                total_useless_expert_rank.append(rank_list_experts[-1])
+                
+                # writer.add_scalar('MoE_Rank', rank_list[args.plt_r], step)
+                # writer.add_scalar('Useless_Expert_Rank', rank_list_experts[-1], step)
+                # total_useless_expert_rank.append(rank_list_experts[-1])
             else:
                 rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample,args.epsilon, moe_training=False,index=1)
                 rank=rank_mlp.rank_mlp()
@@ -156,31 +159,31 @@ def train_loop(X_init, X_bnd, X_f, X_total, u_init, model,loss_fn, optim, args,s
         
         if step % 100 == 0 :
             eval_model(step,X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_training,args,writer)
-        if step % 100 == 0 or step == steps - 1:
-            if moe_training:
-                # rank_moe=epi_rank_moe(model,args.interval,args.integral_sample)
-                # rank=rank_moe.rank_moe()
+        # if step % 100 == 0 or step == steps - 1:
+            # if moe_training:
+            #     # rank_moe=epi_rank_moe(model,args.interval,args.integral_sample)
+            #     # rank=rank_moe.rank_moe()
                 
-                rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample, args.epsilon,moe_training=True,index=1)
-                rank_list=rank_mlp.rank_mlp()
-                total_rank_list.append(rank_list[args.plt_r])
-                rank_list_experts=rank_mlp.experts_rank_mlp()
-                tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss_lbfgs.item():.8f} -rank: {rank_list} \
-                    -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
-                writer.add_scalar('MoE_Loss', total_loss.item(), step)
-                writer.add_scalar('Aux_Loss', aux_loss.item(), step)
-                writer.add_scalar('MoE_Rank', rank_list[args.plt_r], step)
-                writer.add_scalar('Useless_Expert_Rank', rank_list_experts[-1], step)
-                total_useless_expert_rank.append(rank_list_experts[-1])
-            else:
-                rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample,args.epsilon, moe_training=False,index=1)
-                rank=rank_mlp.rank_mlp()
-                total_rank_list.append(rank[args.plt_r])
-                tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss_lbfgs.item():.8f} -rank: {rank}")
-                writer.add_scalar('MLP_Loss', total_loss.item(), step)
+            #     # rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample, args.epsilon,moe_training=True,index=1)
+            #     # rank_list=rank_mlp.rank_mlp()
+            #     # total_rank_list.append(rank_list[args.plt_r])
+            #     # rank_list_experts=rank_mlp.experts_rank_mlp()
+            #     # tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss_lbfgs.item():.8f} -rank: {rank_list} \
+            #     #     -experts_rank: {rank_list_experts[:-2]} -total_experts_rank: {rank_list_experts[-2]} -useless_expert_rank: {rank_list_experts[-1]}")
+            #     # writer.add_scalar('MoE_Loss', total_loss.item(), step)
+            #     # writer.add_scalar('Aux_Loss', aux_loss.item(), step)
+            #     # writer.add_scalar('MoE_Rank', rank_list[args.plt_r], step)
+            #     # writer.add_scalar('Useless_Expert_Rank', rank_list_experts[-1], step)
+            #     # total_useless_expert_rank.append(rank_list_experts[-1])
+            # else:
+            #     rank_mlp=epi_rank_mlp(model,args.x_integral_interval,args.t_integral_interval,args.x_integral_sample, args.t_integral_sample,args.epsilon, moe_training=False,index=1)
+            #     rank=rank_mlp.rank_mlp()
+            #     total_rank_list.append(rank[args.plt_r])
+            #     tqdm.write(f"Step {step+1}/{steps+args.lbfgs_steps} - loss: {loss_lbfgs.item():.8f} -rank: {rank}")
+            #     writer.add_scalar('MLP_Loss', total_loss.item(), step)
         
     
-        
+        # 
     if moe_training: save_model(model,'moe') 
     else:  save_model(model,'mlp')
     return model,total_loss_list,total_rank_list,total_useless_expert_rank
@@ -217,6 +220,7 @@ def eval_model(step,X_init, X_bnd, X_f, X_total, u_init, model, loss_fn,moe_trai
 
     if moe_training:
         tqdm.write("MoE_Model Evaluation Results - step : {:1d}loss: {:.8f}, aux_loss: {:.8f}".format(step,loss.item(), aux_loss.item()))
+        print(f"tau1: {model.moe.tau1.detach().cpu().numpy()}, tau2: {model.moe.tau2.detach().cpu().numpy()}")
     else:
         tqdm.write("MLP_Model Evaluation Results - loss: {:.8f}".format(loss.item()))
 
@@ -319,7 +323,7 @@ def main():
     model,total_loss_list_moe,rank_list_moe,total_useless_expert_rank_moe=train_loop(data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init,\
         model_moe,loss_fn, optimizer, args,args.opt_steps,moe_training=True, writer=writer)
     eval_model(args.opt_steps+args.lbfgs_steps,data_X_init,data_X_bnd,data_X_f,data_X_total,data_u_init, model, loss_fn,moe_training=True,args=args,writer=writer)
-    plot_dual_axis(np.array(total_loss_list_moe),np.array(rank_list_moe),args.opt_steps+args.lbfgs_steps,"moe")
+    # plot_dual_axis(np.array(total_loss_list_moe),np.array(rank_list_moe),args.opt_steps+args.lbfgs_steps,"moe")
     
     # model_mlp=MLP_Model(args.input_size, args.hidden_size,args.depth, args.output_size, activation).to(args.device)
     # optimizer_mlp=get_optimizer(args.optim,model_mlp.parameters(), lr=args.lr)

_fun_run/moe_module/moe.py
@@ -124,15 +124,15 @@ class Gating(nn.Module):
     - num_experts (int): The number of experts.
     - noise_epsilon (float): The noise epsilon value. default is 1e-4.
     """
-    def __init__(self,input_size,num_experts,noise_epsilon=1e-6,gamma=1,R=1):
+    def __init__(self,input_size,num_experts,noise_epsilon=1e-6,gamma=5,R=5):
         super(Gating, self).__init__()
         self.net=nn.Sequential(
             # nn.Linear(input_size,num_experts)
-            nn.Linear(input_size,num_experts*1),
-            # nn.Tanh(),
-            # nn.Linear(num_experts*5,num_experts*5),
+            nn.Linear(input_size,num_experts*5),
             nn.Tanh(),
-            nn.Linear(num_experts*1,num_experts),
+            # nn.Linear(num_experts*5,num_experts*5),
+            # nn.Tanh(),
+            nn.Linear(num_experts*5,num_experts),
             #[I,H]
         )
         self.noisy=nn.Linear(input_size,num_experts)
@@ -153,6 +153,9 @@ class Gating(nn.Module):
             layer.weight.data = gamma * weight
             # 偏置均匀分布在 [0, R]
             layer.bias.data.uniform_(0.0, R)
+            # layer.weight.data = torch.zeros_like(layer.weight)
+            # layer.bias.data.zero_()
+            
             
 
     def forward(self,x,train):
@@ -197,15 +200,19 @@ class MoE(nn.Module):
         self.register_buffer("mean", torch.tensor([0.0]))
         self.register_buffer("std", torch.tensor([1.0]))
         self.gates_check=None
-        self.softmax = nn.Softmax(dim=-1)
+        # self.softmax = nn.Softmax(dim=-1)
+        sfep = 0.05   # 你想要的 ε
+        self.softmax = lambda x: nn.functional.softmax(x / sfep, dim=-1)
         self.gating_network = Gating(self.input_size,self.num_experts)
-        self.tau1 ,self.tau2=torch.tensor(1e-5,requires_grad=True),torch.tensor(1e-5,requires_grad=True)
+        self.alpha1 ,self.alpha2=nn.Parameter(torch.tensor(-2.5)),nn.Parameter(
+            torch.tensor(-3.5))
+        self.alpha1.requires_grad , self.alpha2.requires_grad=False,False
 
     def smoothing(self,step,step_lb):
         # cross train  -> soft
         self.smooth = not self.smooth
         if step >=step_lb:
-            self.smooth =True
+            self.smooth =False
 
         
     def _prob_in_top_k(self, clean_values, noisy_values, noise_stddev, noisy_top_values):
@@ -280,9 +287,10 @@ class MoE(nn.Module):
             # top_logits,_=torch.topk(noisy,k=self.k+1,dim=-1) #values: [k,] indices: [k,] zhou's mode
             # values=values/(values.sum(1, keepdim=True) + 1e-8)  # normalization
             # values= values / (values.sum(1, keepdim=True) + 1e-8) 
-            zeros= torch.zeros_like(noisy, requires_grad=True)
+            zeros= torch.zeros_like(noisy)
             gates=zeros.scatter(1, indices, values)
-            ## softmax--> topk-->normalize
+            # gates= self.softmax(gates)
+            ## softmax--> topk-->normalizess
             # Gating = self.softmax(noisy)
             # values, indices= torch.topk(Gating,k=self.k,dim=-1) 
             # top_logits,_=torch.topk(Gating,k=self.k+1,dim=-1) #values: [k,] indices: [k,]
@@ -301,16 +309,18 @@ class MoE(nn.Module):
             return gates,load
 
     def soft_topk(self,s: torch.Tensor,k: int): #,tau1: float = 5e-2,tau2: float = 1e-2
-        
-        s=F.softmax(s,dim=-1)
+        self.tau1= torch.exp(self.alpha1)
+        self.tau2= torch.exp(self.alpha2)
+        s=self.softmax(s)
         diff = torch.unsqueeze(s,-1) - torch.unsqueeze(s,-2)
         sigma = torch.sigmoid(-diff / self.tau1)
         row_sum = sigma.sum(dim=-1) - 0.5
         r_tilde = 1.0 + row_sum
-        eps=0.5    
+        eps=0.9  
         a = torch.sigmoid((k+eps - r_tilde) / self.tau2)     
 
         a=a*s
+        # a=F.softmax(a,dim=-1)
         # a=a/(a.sum(1, keepdim=True) + 1e-8)
         
 
@@ -542,7 +552,7 @@ class MOE_modify_betap(nn.Module):
                 if m.bias is not None:
                     init.zeros_(m.bias)
     def forward(self, x):
-        x = 2.0 * (x - self.lb) / (self.ub - self.lb) - 1.0
+        # x = 2.0 * (x - self.lb) / (self.ub - self.lb) - 1.0
         output,loss1=self.model(x, self.training)
         # beta=self.Beta(x)
         # output=(output*beta).sum(dim=1,keepdim=True)